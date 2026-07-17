2edf601b645619217f9de88b9a9b73fd6cd7662b
Genesis Escobar <84639113+gescobar19@users.noreply.github.com>
2026-07-17 09:14:38 -0400

    Delete bank-app-java/RohitSingh_CogPractice-SpringbootRESTApiBackend-With-DB-MongoDBCloudAtlas directory

--- bank-app-java/RohitSingh_CogPractice-SpringbootRESTApiBackend-With-DB-MongoDBCloudAtlas/bankapp-java-api-backend/src/main/java/com/example/demo/repos/AccountRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repos;
-
-import com.example.demo.models.Account;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import java.util.List;
-
-public interface AccountRepository extends MongoRepository<Account, String> {
-    List<Account> findByUserId(String userId);
-}
